41271b4881cbfa2f519ab812ca028cbb6f07dedf
Matt Epperson <matt.epperson@kittyhawk.aero>
2020-10-11 11:31:39 -0700

    Dump copter for dumb control engineers.

---
+++ src/Models/DumbCopter.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE ExtendedDefaultRules #-}
+
+
+module Main where
+
+import qualified Graphics.Rendering.Chart.Easy as Plt
+import Graphics.Rendering.Chart.Backend.Diagrams(toFile)
+import Numeric.LinearAlgebra
+import Numeric.GSL.ODE
+
+dynamics :: Double -> [Double] -> [Double]
+dynamics _ [x0, x1, w] =
+    [ x1
+    , -mass*g + k*w + cd*x1**2
+    , (torque - c * w) / j
+    ]
+  where
+    -- timestep
+    dt = 0.01
+    -- Coefficient of drag
+    cd = 0.1
+    -- mass
+    mass = 22
+    -- gravity
+    g = 9.8
+    -- inertia
+    j = 0.05
+    -- Omega to thrust
+    k = 5
+    -- Coefficient of friction motor
+    c = 0.1
+    -- Calculate a physics based torque
+    torque' = mass * g * j / (k * dt) - x1 / dt**2 - w * j / dt + c * w
+    torque = max 0 (min 10 torque')
+
+
+dumbCopter = toFile Plt.def "DumpCopter.svg" $ do
+  let totalTime = 5
+      dt = 0.01
+      ts = (linspace (round $ totalTime / dt) (0, totalTime))
+      sol = odeSolve dynamics [0, -10, 0] ts
+      ([pos, vel, omega]) = toColumns sol
+      ts' = toList ts
+  Plt.plot (Plt.line "pos" [zip ts' (toList pos)])
+  Plt.plot (Plt.line "vel" [zip ts' (toList vel)])
+  Plt.plot (Plt.line "omega" [zip ts' (toList omega)])
+
+main = dumbCopter
